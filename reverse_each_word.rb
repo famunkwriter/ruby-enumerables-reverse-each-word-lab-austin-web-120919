e01d9ba3af66280ee0b6e4bae6c83eb2c493f285
Bretton B Holmes <famunkwriter@me.com>
2019-10-29 12:23:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
 def reverse_each_word = ["Hello there, and how are you?"]
-  reverse_words = []
-  reverse_each_word.each do {|x| print x, " "}
+  reverse_each_word.each do |reverse_word|
   puts reverse_each_word.reverse_each
 end
