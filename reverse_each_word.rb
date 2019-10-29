e214868327a91c81d3ce5c3976e4661ef452f25c
Bretton B Holmes <famunkwriter@me.com>
2019-10-29 12:41:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word ("Hello there, and how are you?")
   original_array = reversed_string.split = (" ")
   return_array = []
-  original_array.each do|string| 
+  original_array.each do|string|
     return_array << string.reverse_each_word
   end
     return_array.join(" ")
